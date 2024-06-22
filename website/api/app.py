c5f24f0ed361489484e91666cb90188cf458b0cd
DevMaycon <dev.maycon08@egamil.com>
2024-06-22 12:55:33 -0300

    feat(api): /getall added.

--- website/api/app.py
+++ website/api/app.py
@@ -1,11 +1,17 @@
 from flask import Flask
 from flask import jsonify
+from json import load, dump
 
 app = Flask(__name__)
 
 @app.route('/')
-def hello():
-    return jsonify('Hello, World!')
+def index_connection():
+    return jsonify({'status': '200', 'message': 'Nice, API is Online!'})
 
-if __name__ == '__main__':
-    app.run(debug=True, port=8080)
+@app.route('/getall')
+def get_all_todo():
+    with open('database') as database_file:
+        content = load(database_file)
+    return jsonify(database_file.keys())
+
+if __name__ == '__main__': app.run(debug=True, port=8080)
