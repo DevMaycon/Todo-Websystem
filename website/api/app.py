426cdeb741e828118e9ec4c89cf44a1e830ff7c4
DevMaycon <dev.maycon08@egamil.com>
2024-06-22 13:50:43 -0300

    feat(api): added new-todo function on backend

--- website/api/app.py
+++ website/api/app.py
@@ -1,19 +1,41 @@
-from flask import Flask
-from flask import jsonify
+from flask import Flask, jsonify, request
 from json import load, dump
 from os import getcwd
 
 app = Flask(__name__)
 path = getcwd()
 
-@app.route('/')
+def database_read():
+    with open(path + '/database.json') as database_file:
+        data = load(database_file)["todo"]
+    return data
+
+def database_write(data):
+    with open(path + '/database.json', 'w') as database_file:
+        dump(data, database_file, indent=4)
+    return 'successful data!'
+
+@app.route('/', methods=['GET'])
 def index_connection():
     return jsonify({'status': '200', 'message': 'Nice, API is Online!'})
 
-@app.route('/getall')
+@app.route('/getall', methods=['GET'])
 def get_all_todo():
-    with open(path + '/database.json') as database_file:
-        content = load(database_file)
-    return jsonify(database_file.keys())
+    return jsonify(database_read().keys())
 
-if __name__ == '__main__': app.run(debug=True, port=8080)
+@app.route('newtodo', methods=['POST'])
+def set_new_todo():
+    # logic to add a new todo to database
+    title, description, autor = (request.json["title"], request.json["description"], request.json["autor"])
+    todo_data = {
+        "id": data["todo"][-1]["id"] + 1,
+        "title": title,
+        "description": description,
+        "autor": autor
+    }
+    data = database_read()
+    data[todo_data['id']+1] = todo_data
+    return jsonify({"message": f"{str(title)} added successfully!", "data": todo_data})
+
+if __name__ == '__main__':
+    app.run(debug=True, port=8080)
