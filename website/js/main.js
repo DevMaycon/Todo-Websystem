0476104920061e6e8299f9af00c8e53341dae8ca
DevMaycon <dev.maycon08@egamil.com>
2024-06-23 18:22:22 -0300

    feat(todo-page): get todos from external API

--- website/js/main.js
+++ website/js/main.js
@@ -10,10 +10,38 @@ function checkItem(item) {
 }
 
 async function todoUpdate() {
-    console.log('run')
-    let request = new XMLHttpRequest;
-    request.open("GET", "https://devmaycon.pythonanywhere.com/getall")
-    let response = await request.send()
-    console.log(response)
+    url = 'https://devmaycon.pythonanywhere.com/getall'
+    let request = await fetch(url).then((res) => (res.json()))
+    setTodos(request)
+}
+
+async function getTodo(id) {
+    url='https://devmaycon.pythonanywhere.com/search/'+id
+    let request = fetch(url)
+    let response = await (await request).json()
     return response
+}
+
+async function setTodos(todos) {
+    let new_todos = []
+    for (let x=0; x < todos.length; x++) {
+        if (todos[x] !== 'last_id') {
+            let todo = await getTodo(todos[x]);
+            new_todos.push(todo);
+            let content = `
+                <div class="todo-item" style="background-color: ${todo.color};">
+                    <div class="todo-info">
+                        <p class="todo-info-title">${todo.title}</p>
+                        <p class="todo-info-description">${todo.description}</p>
+                        <p class="todo-info-autor">${todo.autor}</p>
+                    </div>
+                    <div class="checkbox" style="visibility: ${todo.finished};">
+                        <img src="images/ok_white.png">
+                        <img src="images/ok_white.png">
+                        <img src="images/ok_white.png">
+                    </div>
+                </div>`;
+            document.getElementById('todo').innerHTML += content;
+        }
+    };
 }
